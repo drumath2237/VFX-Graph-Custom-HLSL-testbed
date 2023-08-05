223a3d4d43257431182d1f96ec4d286ca7f08f4b
drumath <2237k.drumath@gmail.com>
2023-08-06 02:36:53 +0900

    :sparkles: 球面上に一様に位置を設定するHLSLロジックの作成

--- Assets/CustomHLSLTestbed/Effects/decrement.hlsl
+++
@@ -1,5 +0,0 @@
-void Rand3Position(inout VFXAttributes attributes, in float3 offset)
-{
-    float3 rand = VFXRAND3;
-    attributes.position = rand + offset;
-}
---
+++ Assets/CustomHLSLTestbed/Effects/positionSphereSurface.hlsl
@@ -0,0 +1,11 @@
+﻿// reference from:
+// https://qiita.com/aa_debdeb/items/e416ae8a018692fc07eb#%E7%90%83%E9%9D%A2%E4%B8%8A%E3%81%AE%E4%B8%80%E6%A7%98%E4%B9%B1%E6%95%B0-1
+void SetPositionOnSphereSurface(inout VFXAttributes attributes, in float3 positionOffset, in float radius)
+{
+    const float pi = 3.14159265;
+    float cosTheta = -2.0 * VFXRAND + 1.0;
+    float sinTheta = sqrt(1.0 - cosTheta * cosTheta);
+    float phi = 2.0 * pi * VFXRAND;
+
+    attributes.position = radius * float3(sinTheta * cos(phi), sinTheta * sin(phi), cosTheta) + positionOffset;
+}
